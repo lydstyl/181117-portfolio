8c19e0c1265478479f7bcff3b40caf71f8cc67b7
Gabriel Brun <lydstyl@gmail.com>
2018-11-18 17:54:31 +0100

    session ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+var cookieSession = require('cookie-session')
+
 const express = require('express')
 const path = require('path');
 const indexRouter = require('./routes/index');
 
-const app = express();
+var bodyParser = require('body-parser')
+
+let app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+  // Cookie Options
+  maxAge: 8 * 60 * 60 * 1000 // 8 hours
+}))
+
+/* give your templates access to your session */
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  next();
+});
+
+// app.use(express.json());
+app.use( bodyParser() );
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,5 +31,5 @@ app.set('view engine', 'pug');
 app.use('/', indexRouter);
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('Listening on port 3000! --> http://localhost:3000/')
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Portfolio Gabriel Brun', message: 'Bienvenu sur mon portfolio!' })
 })
-
 router.get('/page:nb', (req, res) =>{
   res.render('index', { title: 'Portfolio Gabriel Brun', message: `Tu as demandé la page ${req.params.nb}` })
 })
@@ -14,48 +12,18 @@ router.get('/page:nb', (req, res) =>{
 router.get('/admin', (req, res) =>{
   res.render('admin', { title: 'Portfolio admin', message: `Admin` })
 })
-
-
-
 router.post('/admin', (req, res) =>{
-  console.log('ddd');
-  
-  res.redirect('/');
-  // res.render('admin', { title: 'Portfolio admin', message: `Admin` })
+  if (req.body.pass == 'vado') {
+    req.session.admin = 'admin'
+    res.redirect('/'); 
+  }
+  else{
+    res.redirect('/admin')
+  }
 })
-
-// // Create a new customer
-// // POST localhost:3000/cutomer
-// router.post('/customer', (req, res) =>{
-//   if (!req.body) {
-//       return res.status(400).send('Request body is missing!')
-//   }
-
-//   // let user = {
-//   //     name: 'firstname lastname',
-//   //     email: 'email@gmail.com'
-//   // }
-
-//   let model = new CustomerModel(req.body);
-//   model.save()
-//       .then(doc =>{
-//           if(!doc || doc.length === 0) {
-//               return res.status(500).send(doc)
-//           }
-
-//           res.status(201).send(doc) // 201 ressource was created
-//       })
-//       .catch(err => {
-//           res.status(500).json(err) // json instead of send
-//       })
-// })
-
-
-
-
-
-
-
-
+router.get('/logout', (req, res) =>{
+  req.session = null
+  res.redirect('/');
+})
 
 module.exports = router;
