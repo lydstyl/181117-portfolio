48e40ea760c49354ee1a10d0da432e99c3ac853e
lydstyl <lydstyl@gmail.com>
2018-11-25 10:49:42 +0100

    password hash :-)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,35 +2,30 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-
-
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
-  
   const testFolder = path.dirname(require.main.filename) + '/data/';
-  let fs = require('fs');
   let files = fs.readdirSync( testFolder );
   let posts = [];
   for (let i in files) {
     let file = files[i];
     file = fs.readFileSync( path.dirname(require.main.filename) + '/data/' + file, 'utf8' );
     posts.push( JSON.parse(file) )
-
   }
   res.render('index', { title: 'Portfolio Gabriel Brun', message: 'Bienvenu sur mon portfolio!', posts: posts })
 })
-
 
 router.get('/page:nb', (req, res) =>{
   res.render('index', { title: 'Portfolio Gabriel Brun', message: `Tu as demandé la page ${req.params.nb}` })
 })
 
-
 router.get('/admin', (req, res) =>{
   res.render('admin', { title: 'Portfolio admin', message: `Admin` })
 })
+
 router.post('/admin', (req, res) =>{
-  if (req.body.pass == 'vado') {
+  if ( bcrypt.compareSync(req.body.pass, '$2a$10$cwTTXf2EVi3Q3wBHKBC3n.I2BnJFZR9PTAffccpjMtQFhywgRWgba') ) {
     req.session.admin = 'admin'
     res.redirect('/'); 
   }
