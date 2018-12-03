ec07e816613839aabc9c0a329cd2cf62248648ab
Gabriel Brun <lydstyl@gmail.com>
2018-12-03 22:22:01 +0100

    image upload and display ok

--- src/routes/form.js
+++ src/routes/form.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const multer = require('multer');
+const resizeImg = require('resize-img');
+
 const DateId = require('../helper/maintenant.js');
 const getFiles = require('../model/model-post')
-const multer = require('multer');
-
 const uploadDir = './public/img/upload';
+
+const router = express.Router();
 const upload = multer({ dest: uploadDir })
 
 router.get('/add', (req, res) =>{
@@ -35,7 +37,7 @@ router.post('/add', (req, res) =>{
         postCreationDate: date,
         name: req.body.name,
         description: 'my description',
-        imgsrc: 'my/src/img' // https://lydstyl.github.io/CV_WEB_DEV/portfolio/img/html.jpg
+        imgsrc: 'https://lydstyl.github.io/CV_WEB_DEV/portfolio/img/html.jpg'
     }
     json = JSON.stringify(json, '', 3);
     fs.writeFileSync(filePath, json, function(err) {
@@ -55,29 +57,56 @@ router.post('/add', (req, res) =>{
 // U
 
 router.post('/updateimg', upload.single('myimage'), (req, res, next) => {
+    // todo simplify
+
     // req.file is the `myimage` file
     // req.body will hold the text fields, if there were any
     let ext = req.file.originalname.split('.');
     ext = ext[ext.length - 1];
-
     const filename1 = uploadDir + '/' + req.file.filename;
-    const filename2 = uploadDir + '/' + req.body.id + '.' + ext;
-    // todo redim image ici
+    const justFilname2 = req.body.id + '.' + ext
+    const filename2 = uploadDir + '/' + justFilname2;
     fs.rename(filename1, filename2, (err) => {
         if (err) throw err;
         console.log('renamed complete');
     });
+    
+    const upDir = '../public/img/upload/'
+    const imgDir = '../public/img/'
+    const lastJustFileName = req.body.id + '-sm.' + ext
+    file1 = path.join( path.dirname(require.main.filename), upDir, justFilname2 );
+    file2 = path.join( path.dirname(require.main.filename), imgDir, lastJustFileName );
+    
+    resizeImg(fs.readFileSync(file1), {width: 510, height: 340}).then(buf => {
+        fs.writeFileSync( file2, buf);
+    });
+    
+    // update src 
+    const src = 'img/' + lastJustFileName
+    const filePath = path.dirname(require.main.filename) + '/data/' + req.body.id + '.json';
+    let json = fs.readFileSync( filePath );
+    json = JSON.parse(json)
+    json.imgsrc = src;
+    json = JSON.stringify(json);
+    fs.writeFileSync(filePath, json, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+
     res.redirect('/');
 })
 
 router.post('/update', (req, res) =>{
+    // todo update uniquement les infos qu'on a exemple ne pas update imgsrc ... name doit pas etre required ici
     const filePath = path.dirname(require.main.filename) + '/data/' + req.body.id + '.json';
     let json = {
         id: req.body.id,
         filePath: filePath,
         name: req.body.name,
         description: 'my description',
-        imgsrc: 'my/src/img'
+        imgsrc: 'https://lydstyl.github.io/CV_WEB_DEV/portfolio/img/html.jpg'
     }
     json = JSON.stringify(json, '', 3);
     
@@ -100,7 +129,7 @@ router.get('/edit/:id', (req, res) =>{
 
 // D
 router.get('/del/:id', (req, res) =>{
-    fs.unlinkSync( path.dirname(require.main.filename) + '/data/' + req.params.id + '.json' );
+    fs.unlinkSync( path.join(path.dirname(require.main.filename) , 'data' , req.params.id + '.json') );
     res.redirect('/'); 
 })
 
