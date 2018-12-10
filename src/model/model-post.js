4c4e6196a4bcb0e560021b1a93b65d9a507fd84e
Gabriel Brun <lydstyl@gmail.com>
2018-12-10 22:14:59 +0100

    fix mkdir data if not existing

--- src/model/model-post.js
+++ src/model/model-post.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 //const descendingSort = require('../helper/descending-sort.js')
 
 function initFirstPost() {
@@ -11,36 +11,39 @@ function initFirstPost() {
         "description": "my description",
         "imgsrc": "https://lydstyl.github.io/CV_WEB_DEV/portfolio/img/html.jpg"
      }
-    json = JSON.stringify(json, '', 3);
+    json = JSON.stringify(json, '', 3)
     fs.writeFileSync( path.join(folder, 'initFirstPost.json'), json, (err) => {
         if(err) {
-            return console.log(err);
+            return console.log(err)
         }
-        console.log("The file was saved!");
-    }); 
+        console.log("The file was saved!")
+    }) 
 }
 function writeInitPositions() {
-    const json = JSON.stringify(["initFirstPost.json"], '', 3);
+    const json = JSON.stringify(["initFirstPost.json"], '', 3)
     fs.writeFileSync( path.join(__dirname, '../data/positions.json'), json, (err) => {
         if(err) {
-            return console.log(err);
+            return console.log(err)
         }
-        console.log("The file was saved!");
-    }); 
+        console.log("The file was saved!")
+    }) 
 }
 function getPagesNumber(items, pageIems) {
-    let tmp = items / pageIems;
+    let tmp = items / pageIems
     tmp = tmp.toString().split('.')
-    let pages = tmp[0];
+    let pages = tmp[0]
     if (tmp[1] > 0) {
-        pages ++;
+        pages ++
     }
-    return pages;
+    return pages
 }
 
 function getFiles() {
-    const dataFolder = path.join( path.dirname(require.main.filename), 'data');
-    return fs.readdirSync( dataFolder );
+    const dataFolder = path.join( path.dirname(require.main.filename), 'data')
+    if (!fs.existsSync(dataFolder)) {
+        fs.mkdirSync(dataFolder)
+    }
+    return fs.readdirSync( dataFolder )
 }
 function getPositions() {
     const positions = path.join(__dirname, '../data', 'positions.json')
@@ -66,13 +69,13 @@ function getFilesForPage( itemsByPage, pageNumber ) {
 }
 
 function getPosts(req) {
-    let PageNum = 1;
+    let PageNum = 1
     if (req.params.nb) {
-        PageNum = req.params.nb;
+        PageNum = req.params.nb
     }
     let filesAndPageNb = getFilesForPage( 9, PageNum )
 
-    let posts = [];
+    let posts = []
     for (let i in filesAndPageNb.files) {
         let file = filesAndPageNb.files[i]
         let filePath = path.join(path.dirname(require.main.filename), 'data', file )
