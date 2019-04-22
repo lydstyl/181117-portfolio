3a837ff1963d3d72a9a7ab190310b2000df5e7dc
Gabriel Brun <lydstyl@gmail.com>
2019-04-22 17:23:00 +0200

    prepare for heroku

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-const express = require('express')
-const path = require('path');
-const cookieSession = require('cookie-session')
+const express = require("express");
+const path = require("path");
+const cookieSession = require("cookie-session");
 
-const indexRouter = require('./routes/index');
-const form = require('./routes/form');
+const indexRouter = require("./routes/index");
+const form = require("./routes/form");
 
 let app = express();
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1'],
-  // Cookie Options
-  maxAge: 3 * 60 * 60 * 1000 // 3 hours
-}))
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
+    // Cookie Options
+    maxAge: 3 * 60 * 60 * 1000 // 3 hours
+  })
+);
 
 /* give your templates access to your session */
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
   res.locals.session = req.session;
   next();
 });
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // view engine setup
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // Note: you must place sass-middleware *before* `express.static` or else it will
 // not work.
-app.use( express.static( path.join(__dirname, '../public') ) );
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use('/', indexRouter);
-app.use('/form', form);
+app.use("/", indexRouter);
+app.use("/form", form);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Listening on port ' + port + '! --> http://localhost:' + port + '/')
-})
+  console.log(
+    "Listening on port " + port + "! --> http://localhost:" + port + "/"
+  );
+});
